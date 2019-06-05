19f44889c8192d86899d46deb61010029e7fd880
Maciej Gołaszewski <jodator@jodator.net>
2019-06-05 12:29:15 +0200

    wip: ckeditor5#1731 - remove useless nulls.

--- src/ui/mediaformview.js
+++ src/ui/mediaformview.js
@@ -153,16 +153,13 @@ export default class MediaFormView extends View {
 
 	destroy() {
 		super.destroy();
-		this.focusTracker && this.focusTracker && this.focusTracker.destroy(); this.focusTracker = null;
-		this.keystrokes && this.keystrokes && this.keystrokes.destroy(); this.keystrokes = null;
-		this._focusCycler && this._focusCycler && this._focusCycler.destroy(); this._focusCycler = null;
+		this.focusTracker.destroy();
+		this.keystrokes.destroy();
+		this._focusCycler.destroy();
 
-		this.urlInputView && this.urlInputView.destroy();
-		this.urlInputView = null;
-		this.saveButtonView && this.saveButtonView.destroy();
-		this.saveButtonView = null;
-		this.cancelButtonView && this.cancelButtonView.destroy();
-		this.cancelButtonView = null;
+		this.urlInputView.destroy();
+		this.saveButtonView.destroy();
+		this.cancelButtonView.destroy();
 	}
 
 	/**
