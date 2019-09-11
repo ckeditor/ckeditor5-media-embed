94aca984cbcda5c5b66eb88e001e8e282a466502
Jonathan Whitney <jonathan.whitney@salesforce.com>
2019-09-11 13:51:44 -0400

    Mod package.json to make specific to SolDes / Added media embed option for Vidyard embeds

--- src/mediaembedediting.js
+++ src/mediaembedediting.js
@@ -32,125 +32,31 @@ export default class MediaEmbedEditing extends Plugin {
 	/**
 	 * @inheritDoc
 	 */
-	constructor( editor ) {
-		super( editor );
+	constructor(editor) {
+		super(editor);
 
-		editor.config.define( 'mediaEmbed', {
+		editor.config.define('mediaEmbed', {
 			providers: [
 				{
-					name: 'dailymotion',
-					url: /^dailymotion\.com\/video\/(\w+)/,
+					name: 'vidyard',
+					url: /^salesforce\.vidyard\.com\/watch\/(\d+)/,
 					html: match => {
-						const id = match[ 1 ];
+						const id = match[1];
 
 						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; ">' +
-								`<iframe src="https://www.dailymotion.com/embed/video/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" width="480" height="270" allowfullscreen allow="autoplay">' +
-								'</iframe>' +
-							'</div>'
+							`<iframe class="vidyard_iframe" src="//play.vidyard.com/${id}.html?v=3.1.1" width="640" height="360" scrolling="no" frameborder="0" allowtransparency="true" allowfullscreen></iframe>`
 						);
 					}
-				},
-
-				{
-					name: 'spotify',
-					url: [
-						/^open\.spotify\.com\/(artist\/\w+)/,
-						/^open\.spotify\.com\/(album\/\w+)/,
-						/^open\.spotify\.com\/(track\/\w+)/
-					],
-					html: match => {
-						const id = match[ 1 ];
-
-						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 126%;">' +
-								`<iframe src="https://open.spotify.com/embed/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" allowtransparency="true" allow="encrypted-media">' +
-								'</iframe>' +
-							'</div>'
-						);
-					}
-				},
-
-				{
-					name: 'youtube',
-					url: [
-						/^(?:m\.)?youtube\.com\/watch\?v=([\w-]+)/,
-						/^(?:m\.)?youtube\.com\/v\/([\w-]+)/,
-						/^youtube\.com\/embed\/([\w-]+)/,
-						/^youtu\.be\/([\w-]+)/
-					],
-					html: match => {
-						const id = match[ 1 ];
-
-						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-								`<iframe src="https://www.youtube.com/embed/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
-								'</iframe>' +
-							'</div>'
-						);
-					}
-				},
-
-				{
-					name: 'vimeo',
-					url: [
-						/^vimeo\.com\/(\d+)/,
-						/^vimeo\.com\/[^/]+\/[^/]+\/video\/(\d+)/,
-						/^vimeo\.com\/album\/[^/]+\/video\/(\d+)/,
-						/^vimeo\.com\/channels\/[^/]+\/(\d+)/,
-						/^vimeo\.com\/groups\/[^/]+\/videos\/(\d+)/,
-						/^vimeo\.com\/ondemand\/[^/]+\/(\d+)/,
-						/^player\.vimeo\.com\/video\/(\d+)/
-					],
-					html: match => {
-						const id = match[ 1 ];
-
-						return (
-							'<div style="position: relative; padding-bottom: 100%; height: 0; padding-bottom: 56.2493%;">' +
-								`<iframe src="https://player.vimeo.com/video/${ id }" ` +
-									'style="position: absolute; width: 100%; height: 100%; top: 0; left: 0;" ' +
-									'frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>' +
-								'</iframe>' +
-							'</div>'
-						);
-					}
-				},
-
-				{
-					name: 'instagram',
-					url: /^instagram\.com\/p\/(\w+)/
-				},
-				{
-					name: 'twitter',
-					url: /^twitter\.com/
-				},
-				{
-					name: 'googleMaps',
-					url: /^google\.com\/maps/
-				},
-				{
-					name: 'flickr',
-					url: /^flickr\.com/
-				},
-				{
-					name: 'facebook',
-					url: /^facebook\.com/
 				},
 			]
-		} );
+		});
 
 		/**
 		 * The media registry managing the media providers in the editor.
 		 *
 		 * @member {module:media-embed/mediaregistry~MediaRegistry} #registry
 		 */
-		this.registry = new MediaRegistry( editor.locale, editor.config.get( 'mediaEmbed' ) );
+		this.registry = new MediaRegistry(editor.locale, editor.config.get('mediaEmbed'));
 	}
 
 	/**
@@ -161,89 +67,89 @@ export default class MediaEmbedEditing extends Plugin {
 		const schema = editor.model.schema;
 		const t = editor.t;
 		const conversion = editor.conversion;
-		const renderMediaPreview = editor.config.get( 'mediaEmbed.previewsInData' );
+		const renderMediaPreview = editor.config.get('mediaEmbed.previewsInData');
 		const registry = this.registry;
 
-		editor.commands.add( 'mediaEmbed', new MediaEmbedCommand( editor ) );
+		editor.commands.add('mediaEmbed', new MediaEmbedCommand(editor));
 
 		// Configure the schema.
-		schema.register( 'media', {
+		schema.register('media', {
 			isObject: true,
 			isBlock: true,
 			allowWhere: '$block',
-			allowAttributes: [ 'url' ]
-		} );
+			allowAttributes: ['url']
+		});
 
 		// Model -> Data
-		conversion.for( 'dataDowncast' ).elementToElement( {
+		conversion.for('dataDowncast').elementToElement({
 			model: 'media',
-			view: ( modelElement, viewWriter ) => {
-				const url = modelElement.getAttribute( 'url' );
+			view: (modelElement, viewWriter) => {
+				const url = modelElement.getAttribute('url');
 
-				return createMediaFigureElement( viewWriter, registry, url, {
+				return createMediaFigureElement(viewWriter, registry, url, {
 					renderMediaPreview: url && renderMediaPreview
-				} );
+				});
 			}
-		} );
+		});
 
 		// Model -> Data (url -> data-oembed-url)
-		conversion.for( 'dataDowncast' ).add(
-			modelToViewUrlAttributeConverter( registry, {
+		conversion.for('dataDowncast').add(
+			modelToViewUrlAttributeConverter(registry, {
 				renderMediaPreview
-			} ) );
+			}));
 
 		// Model -> View (element)
-		conversion.for( 'editingDowncast' ).elementToElement( {
+		conversion.for('editingDowncast').elementToElement({
 			model: 'media',
-			view: ( modelElement, viewWriter ) => {
-				const url = modelElement.getAttribute( 'url' );
-				const figure = createMediaFigureElement( viewWriter, registry, url, {
+			view: (modelElement, viewWriter) => {
+				const url = modelElement.getAttribute('url');
+				const figure = createMediaFigureElement(viewWriter, registry, url, {
 					renderForEditingView: true
-				} );
+				});
 
-				return toMediaWidget( figure, viewWriter, t( 'media widget' ) );
+				return toMediaWidget(figure, viewWriter, t('media widget'));
 			}
-		} );
+		});
 
 		// Model -> View (url -> data-oembed-url)
-		conversion.for( 'editingDowncast' ).add(
-			modelToViewUrlAttributeConverter( registry, {
+		conversion.for('editingDowncast').add(
+			modelToViewUrlAttributeConverter(registry, {
 				renderForEditingView: true
-			} ) );
+			}));
 
 		// View -> Model (data-oembed-url -> url)
-		conversion.for( 'upcast' )
+		conversion.for('upcast')
 			// Upcast semantic media.
-			.elementToElement( {
+			.elementToElement({
 				view: {
 					name: 'oembed',
 					attributes: {
 						url: true
 					}
 				},
-				model: ( viewMedia, modelWriter ) => {
-					const url = viewMedia.getAttribute( 'url' );
+				model: (viewMedia, modelWriter) => {
+					const url = viewMedia.getAttribute('url');
 
-					if ( registry.hasMedia( url ) ) {
-						return modelWriter.createElement( 'media', { url } );
+					if (registry.hasMedia(url)) {
+						return modelWriter.createElement('media', { url });
 					}
 				}
-			} )
+			})
 			// Upcast non-semantic media.
-			.elementToElement( {
+			.elementToElement({
 				view: {
 					name: 'div',
 					attributes: {
 						'data-oembed-url': true
 					}
 				},
-				model: ( viewMedia, modelWriter ) => {
-					const url = viewMedia.getAttribute( 'data-oembed-url' );
+				model: (viewMedia, modelWriter) => {
+					const url = viewMedia.getAttribute('data-oembed-url');
 
-					if ( registry.hasMedia( url ) ) {
-						return modelWriter.createElement( 'media', { url } );
+					if (registry.hasMedia(url)) {
+						return modelWriter.createElement('media', { url });
 					}
 				}
-			} );
+			});
 	}
 }
